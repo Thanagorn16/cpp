ca0c566ba51a8b776e7221d2214a65bd91827b05
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2023-09-03 18:07:42 +0700

    done mod03 ex00

---
+++ module-03/ex00/ClapTrap.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 13:11:44 by prachman          #+#    #+#             */
+/*   Updated: 2023/09/03 18:07:14 by prachman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoint(10), _energyPoint(10), _attDmg(0)
+{
+    std::cout << "Created ClapTrap: " << _name << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& obj) : 
+    _name(obj._name), _hitPoint(obj._hitPoint), _energyPoint(obj._energyPoint), _attDmg(obj._attDmg)
+{
+    std::cout << "Created a clone of " << obj._name << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& obj)
+{
+    std::cout << "Copy assignment operator" << std::endl;
+    if (this != &obj)
+    {
+        this->_name = obj._name;
+        this->_hitPoint = obj._hitPoint;
+        this->_energyPoint = obj._energyPoint;
+        this->_attDmg = obj._attDmg;
+    }
+    return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+void    ClapTrap::attack(const std::string& target)
+{
+    if (_energyPoint == 0)
+    {
+        std::cout << "ClapTrap " << _name << " has no energy" << std::endl;
+        return ;
+    }
+    --_energyPoint;
+    std::cout << "ClapTrap " << _name << " attacked " << target << std::endl;
+    std::cout << "it has caused " << _attDmg << " damage" << std::endl;
+    std::cout << "it has " << _energyPoint << " energy left" << std::endl;
+}
+
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+    _hitPoint -= amount;
+    std::cout << "ClapTrap " << _name << " took " << amount << " damage" << std::endl;
+    if (_hitPoint > 0)
+        std::cout << "it has " << _hitPoint << " health left" << std::endl;
+    else
+        std::cout << "ClapTrap " << _name << " has died" << std::endl;
+}
+
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    if (_energyPoint == 0)
+    {
+        std::cout << "ClapTrap " << _name << " has no energy" << std::endl;
+        return ;
+    }
+    _hitPoint += amount;
+    std::cout << "ClapTrap " << _name << " has been repaired" << std::endl;
+    std::cout << "it has " << _hitPoint << " health left" << std::endl;
+}
---
+++ module-03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 11:39:56 by prachman          #+#    #+#             */
+/*   Updated: 2023/09/03 13:09:01 by prachman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <iostream>
+
+class   ClapTrap
+{
+    private:
+        std::string     _name;
+        unsigned int    _hitPoint; //health of the ClapTrap
+        unsigned int    _energyPoint;
+        unsigned int    _attDmg;
+    
+    public:
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap& obj);
+        ClapTrap& operator=(const ClapTrap& obj);
+        ~ClapTrap(void);
+
+        void    attack(const std::string& target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+};
+
+#endif
