ed1ba60297b7cc1246c14b4cee0ff0494e05f877
Thanagorn Ruangsit <truangsi@z2t16c1.42bangkok.com>
2023-09-06 11:45:25 +0700

    push

--- module-03/ex00/main.cpp
+++ module-03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: truangsi <truangsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 13:11:19 by prachman          #+#    #+#             */
-/*   Updated: 2023/09/03 18:04:01 by prachman         ###   ########.fr       */
+/*   Updated: 2023/09/06 11:03:05 by truangsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int main()
 {
-    ClapTrap    clap1("Cap");
-    
-    clap1.attack("a tree");
-    clap1.takeDamage(10);
-    clap1.beRepaired(10);
+	ClapTrap	clap1("Cap");
 
-    return (0);
+	clap1.attack("a tree");
+	clap1.takeDamage(10);
+	clap1.beRepaired(10);
+
+	return (0);
 }
