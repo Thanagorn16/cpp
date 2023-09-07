654edb88e8cdcc54e2cf7cf1a40a73012c21d86b
Thanagorn Ruangsit <truangsi@z2t16c1.42bangkok.com>
2023-09-07 15:32:25 +0700

    delete virtual in WrongAnimal

--- module-04/ex01/WrongAnimal.hpp
+++ module-04/ex01/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: truangsi <truangsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:06:20 by truangsi          #+#    #+#             */
-/*   Updated: 2023/09/06 11:20:02 by truangsi         ###   ########.fr       */
+/*   Updated: 2023/09/07 15:27:15 by truangsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ class	WrongAnimal
 		WrongAnimal(std::string type);
 		WrongAnimal(const WrongAnimal& obj);
 		WrongAnimal& operator=(const WrongAnimal& obj);
-		virtual	~WrongAnimal(void);
+		~WrongAnimal(void);
 
 		std::string		getType(void) const;
-		virtual void	makeSound(void) const;
+		void	makeSound(void) const;
 };
 
 #endif
