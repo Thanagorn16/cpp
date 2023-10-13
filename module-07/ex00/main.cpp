7e8090f258a961f21576c8a6ff336ce1042c8c01
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2023-10-13 09:13:50 +0700

    edited mex00/main

--- module-07/ex00/main.cpp
+++ module-07/ex00/main.cpp
@@ -6,32 +6,23 @@
 /*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 09:01:54 by prachman          #+#    #+#             */
-/*   Updated: 2023/10/13 09:10:25 by prachman         ###   ########.fr       */
+/*   Updated: 2023/10/13 09:13:12 by prachman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
-
-int	main()
-{
-	int	a = 1;
-	int	b = 2;
-	int	c = 1;
-	int	d;
-
-	swap(a, b);
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
-	swap(a, b);
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
-	
-	d = min(a, b);
-	std::cout << "d: " << d << std::endl;
-	d = max(a, b);
-	std::cout << "d: " << d << std::endl;
-	d = max(a, c);
-	std::cout << "d: " << d << std::endl;
-
-	return (0);
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
