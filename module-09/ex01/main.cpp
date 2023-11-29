60d2f38eefaf04a6da52f675e03c49f197388c26
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2023-11-29 16:16:21 +0700

    ex01: all the test work fine. However, possible error still left to be handled

--- module-09/ex01/main.cpp
+++ module-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:13:49 by prachman          #+#    #+#             */
-/*   Updated: 2023/11/29 16:07:25 by prachman         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:13:34 by prachman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,9 +58,6 @@ int main(int ac, char **av)
 {
 	std::stack<int>	stack;
 	std::string 	arg = static_cast<std::string>(av[1]);
-	// int				first;
-	// int				second;
-	// int				res;
 
 	if (ac != 2)
 		return (std::cout << "Error: invalid argument" << std::endl, 0);
@@ -88,48 +85,17 @@ int main(int ac, char **av)
 			i += len;
 		}
 		int	res = makeStack(arg[i], stack, num);
-		if (res == 1)
+		if (res == 1) //if stack stores number
 			continue ;
-		else if (res == 2)
+		else if (res == 2) // if stack makes calculation
 		{
 			if (stack.size() == 2 && arg[i + 1] != '\0')
-			{
-				// std::cout << arg[i] << std::endl;
 				continue ;
-			}
 			if (stack.size() > 1)
-				return (std::cout << "Error: invalida" << std::endl, 0);
-			continue ;
+				return (std::cout << "Error: invalid" << std::endl, 0);
 		}
 		else
 			return 0;
-		// if (arg[i] == ' ') // store number
-		// {
-		// 	stack.push(num);
-		// 	continue ;
-		// }
-		// else if (arg[i] == '+' || arg[i] == '-' || arg[i] == '*' || arg[i] == '/')
-		// {
-		// 	first = stack.top();
-		// 	stack.pop();
-		// 	if (stack.empty()) // case of operator after operator but no element to do the operation with
-		// 		return (std::cout << "Error: invalid inside" << std::endl, 0);
-		// 	second = stack.top();
-		// 	stack.pop();
-		// 	res = doOperation(second, first, arg[i]);
-		// 	stack.push(res);
-		// 	// std::cout << stack.top() << std::endl;
-		// 	// std::cout << stack.size() << std::endl;
-		// }
-		// else
-		// 	return (std::cout << "Error: invalid character " << arg[i] << std::endl, 0);
-		// if (stack.size() == 2 && arg[i + 1] != '\0')
-		// {
-		// 	std::cout << arg[i] << std::endl;
-		// 	continue ;
-		// }
-		// if (stack.size() > 1)
-		// 	return (std::cout << "Error: invalid" << std::endl, 0);
 	}
 	std::cout << stack.top() << std::endl;
 }
