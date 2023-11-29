d3075de27b34ac2d46e8d9ed79e859b6f15d37b0
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2023-11-29 16:08:57 +0700

    ex01 with old version comment

--- module-09/ex01/main.cpp
+++ module-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: prachman <prachman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:13:49 by prachman          #+#    #+#             */
-/*   Updated: 2023/11/29 12:47:16 by prachman         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:07:25 by prachman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,44 @@ int	doOperation(int a, int b, char op)
 	}
 }
 
+int	makeStack(char c, std::stack<int> &stack, int num )
+{
+	int				first;
+	int				second;
+	int				res;
+
+	if (c == ' ') // store number
+		stack.push(num);
+	else if (c == '+' || c == '-' || c == '*' || c == '/')
+	{
+		first = stack.top();
+		stack.pop();
+		if (stack.empty()) // case of operator after operator but no element to do the operation with
+			return (std::cout << "Error: invalid" << std::endl, 0);
+		second = stack.top();
+		stack.pop();
+		res = doOperation(second, first, c);
+		stack.push(res);
+		return 2;
+	}
+	else
+		return (std::cout << "Error: invalid character " << c << std::endl, 0);
+	return (1);
+}
+
 int main(int ac, char **av)
 {
 	std::stack<int>	stack;
 	std::string 	arg = static_cast<std::string>(av[1]);
-	int				first;
-	int				second;
-	int				res;
+	// int				first;
+	// int				second;
+	// int				res;
 
 	if (ac != 2)
 		return (std::cout << "Error: invalid argument" << std::endl, 0);
 	for (size_t i = 0; i < arg.length(); i++)
 	{
-		int	num;
+		int	num = 0;
 		if (arg[i] == ' ')
 			continue ;
 		if (isdigit(arg[i]))
@@ -57,24 +82,54 @@ int main(int ac, char **av)
 				std::stringstream	converter;
 				converter << str;
 				converter >> num;
-				// std::cout << num << std::endl;
+				if (num > 10)
+					return (std::cout << "Error: invalid number: " << num << std::endl, 0);
 			}
 			i += len;
 		}
-		if (arg[i] == ' ')
-			stack.push(num);
-		else if (arg[i] == '+' || arg[i] == '-' || arg[i] == '*' || arg[i] == '/')
+		int	res = makeStack(arg[i], stack, num);
+		if (res == 1)
+			continue ;
+		else if (res == 2)
 		{
-			first = stack.top();
-			stack.pop();
-			second = stack.top();
-			stack.pop();
-			res = doOperation(second, first, arg[i]);
-			stack.push(res);
-			// std::cout << stack.top() << std::endl;
+			if (stack.size() == 2 && arg[i + 1] != '\0')
+			{
+				// std::cout << arg[i] << std::endl;
+				continue ;
+			}
+			if (stack.size() > 1)
+				return (std::cout << "Error: invalida" << std::endl, 0);
+			continue ;
 		}
 		else
-			return (std::cout << "Error: invalid character " << arg[i] << std::endl, 0);
-		
+			return 0;
+		// if (arg[i] == ' ') // store number
+		// {
+		// 	stack.push(num);
+		// 	continue ;
+		// }
+		// else if (arg[i] == '+' || arg[i] == '-' || arg[i] == '*' || arg[i] == '/')
+		// {
+		// 	first = stack.top();
+		// 	stack.pop();
+		// 	if (stack.empty()) // case of operator after operator but no element to do the operation with
+		// 		return (std::cout << "Error: invalid inside" << std::endl, 0);
+		// 	second = stack.top();
+		// 	stack.pop();
+		// 	res = doOperation(second, first, arg[i]);
+		// 	stack.push(res);
+		// 	// std::cout << stack.top() << std::endl;
+		// 	// std::cout << stack.size() << std::endl;
+		// }
+		// else
+		// 	return (std::cout << "Error: invalid character " << arg[i] << std::endl, 0);
+		// if (stack.size() == 2 && arg[i + 1] != '\0')
+		// {
+		// 	std::cout << arg[i] << std::endl;
+		// 	continue ;
+		// }
+		// if (stack.size() > 1)
+		// 	return (std::cout << "Error: invalid" << std::endl, 0);
 	}
+	std::cout << stack.top() << std::endl;
 }
